0465dec9b294725408d3fbf6c1f9ef6f4b67cc90
Kanstantsin Adzerykha <kanstantsin_adzerykha@epam.com>
2022-07-15 09:33:39 +0300

    task-3

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,14 +1,15 @@
 import * as Yup from 'yup';
 
 export type Product = {
-  id: string,
-  title: string,
-  description: string,
-  price: number,
+  id: string;
+  title: string;
+  image: string;
+  price: number;
+  description: string;
 };
 
 export const ProductSchema = Yup.object().shape({
   title: Yup.string().required(),
   description: Yup.string(),
-  price: Yup.number().required(),
+  price: Yup.number().required()
 });
