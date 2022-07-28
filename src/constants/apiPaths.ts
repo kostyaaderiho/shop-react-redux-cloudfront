ec70c4300019867284976dded205f22e73d22b35
Kanstantsin Adzerykha <kanstantsin_adzerykha@epam.com>
2022-07-28 15:48:46 +0300

    Create product

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
     'https://kcob8mkgah.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://kcob8mkgah.execute-api.eu-west-1.amazonaws.com/dev/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
