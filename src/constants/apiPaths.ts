9eae05f6a78b0dc98324ec0b88f0fcad1d2f5868
Kanstantsin Adzerykha <kanstantsin_adzerykha@epam.com>
2022-07-30 01:04:46 +0300

    Initial changes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product:
     'https://kcob8mkgah.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vd3cvn2lv2.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://kcob8mkgah.execute-api.eu-west-1.amazonaws.com/dev/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
